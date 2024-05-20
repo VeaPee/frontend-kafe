284a69d581264473cc6e064ced77d6b28b2675c4
Vincentius Agung Prabandaru <vap.agung@gmail.com>
2024-05-21 05:41:58 +0700

    fix port

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -8,4 +8,5 @@ REACT_APP_BACKEND_URL=$(aws ssm get-parameters --region ap-southeast-2 --names R
 echo "REACT_APP_BACKEND_URL: $REACT_APP_BACKEND_URL"
 
 # Run the Docker image as a container
-docker run -dit -p -e REACT_APP_BACKEND_URL="$REACT_APP_BACKEND_URL" 80:80 veapee/frontend-kafe
+docker run -dit -p 80:80 -e REACT_APP_BACKEND_URL="$REACT_APP_BACKEND_URL" veapee/frontend-kafe
+
