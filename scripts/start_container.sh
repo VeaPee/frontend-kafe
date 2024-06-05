ea28237fca13eabb14b6429e50a97ef974c3b3c0
Vincentius Agung Prabandaru <vap.agung@gmail.com>
2024-06-05 22:47:03 +0700

    edit container

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -8,5 +8,5 @@ REACT_APP_BACKEND_URL=$(aws ssm get-parameters --region ap-southeast-2 --names R
 echo "REACT_APP_BACKEND_URL: $REACT_APP_BACKEND_URL"
 
 # Run the Docker image as a container
-docker run -dit -p 80:80 -e REACT_APP_BACKEND_URL="$REACT_APP_BACKEND_URL" veapee/frontend-kafe
+docker run -dit -p 80:3000 -e REACT_APP_BACKEND_URL="$REACT_APP_BACKEND_URL" veapee/frontend-kafe
 
--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,5 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-# Stop the running container (if any)
-echo "Hi"
+# Check if there are any running containers
+if [ "$(docker ps -q)" ]; then
+    # Stop all running containers
+    echo "Stopping all running Docker containers..."
+    docker stop $(docker ps -q)
+
+    # # Optionally, you can also remove all stopped containers
+    # docker rm $(docker ps -a -q)
+    
+    echo "All Docker containers have been stopped."
+else
+    echo "No running Docker containers found."
+fi
