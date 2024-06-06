8e365f7357172c3cb25c20e5c3ad48aea4540638
Vincentius Agung Prabandaru <vap.agung@gmail.com>
2024-06-06 20:43:54 +0700

    Setting CPU and Memory Limits EC2

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,8 +5,11 @@ set -e
 docker pull veapee/frontend-kafe:latest
 
 REACT_APP_BACKEND_URL=$(aws ssm get-parameters --region ap-southeast-2 --names REACT_APP_BACKEND_URL --with-decryption --query Parameters[0].Value --output text)
-echo "REACT_APP_BACKEND_URL: $REACT_APP_BACKEND_URL"
 
 # Run the Docker image as a container
-docker run -dit -p 80:3000 -e REACT_APP_BACKEND_URL="$REACT_APP_BACKEND_URL" veapee/frontend-kafe
+docker run -dit -p 80:3000 \
+    --cpus="0.5" \
+    --memory="512m" \
+    -e REACT_APP_BACKEND_URL="$REACT_APP_BACKEND_URL" \
+    veapee/frontend-kafe
 
